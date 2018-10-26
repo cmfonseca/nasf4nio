25e0c2c3808187795d7386755ff17ee0cba901f0
Carlos M. Fonseca <44481304+cmfonseca@users.noreply.github.com>
2018-10-26 14:36:47 +0100

    Create knapsack.h

---
+++ src/knapsack/knapsack.h
@@ -0,0 +1,28 @@
+/* knapsack.h
+ *
+ * (C) 2018 Eva Tuba <etuba@ieee.org> and Carlos M. Fonseca <cmfonsec@dei.uc.pt>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "problem.h"
+
+/* This header file contains all problem dependent definitions */
+
+/* Problem-specific instantiation
+ *
+ * Input file format as described in http://artemisa.unicauca.edu.co/~johnyortega/instances_01_KP/
+ */
+struct problem *newProblem(const char *filename);
