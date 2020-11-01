38f6bd67e35b8b83468d3e341e24b50be9c8aa87
husticfilip <filip.hustic@fer.hr>
2020-11-01 20:32:52 +0100

    findSuitableBreakPoint added check of element left to the breakpoint seg->bpAtPosition removed being redundant, consequent change to the code done

--- src/symmtsp/symmtsp-sga.c
+++ src/symmtsp/symmtsp-sga.c
@@ -15,50 +15,46 @@ int main(int argc, char **argv) {
     double cost, mincost;
     char *filename;
 
-        /* Set up random number generation */
-        rng = gsl_rng_alloc(gsl_rng_mt19937);
-        gsl_rng_set(rng, time(0));
-        test();
-//   if (argc < 4) {
-//        fprintf(stderr, "Usage: %s <board size> <pop size> <max iter>\n", argv[0]);
-//        return 0;
-//    }
-//
-//    /* Set up random number generation */
-//    rng = gsl_rng_alloc(gsl_rng_mt19937);
-//    gsl_rng_set(rng, time(0));
-//
-//    /* Input arguments */
-//    filename = argv[1];
-//    pop_size = atoi(argv[2]);
-//    max_iter = atoi(argv[3]);
-//
-//    /* Problem and solver instantiation */
-//    p = newProblem(filename);
-//    if (p != NULL) {
-//        ss = newSolver(p, pop_size);
-//
-//        /* Run */
-//        getObjectiveVector(&mincost, getSolverSolution(ss));
-//        printf("iter = 0, obj = %.0f\n", mincost);
-//        for (i = 0; mincost > 0 && i < max_iter; i++) {
-//            nextSolverState(ss);
-//            getObjectiveVector(&cost, getSolverSolution(ss));
-//            if (cost < mincost) {
-//                mincost = cost;
-//                printf("iter = %d, obj = %.0f\n", i+1, mincost);
-//            }
-//        }
-//
-//        /* Report result */
-//        printSolution(getSolverSolution(ss));
-//
-//        /* Clean up */
-//        freeSolver(ss);
-//        freeProblem(p);
-//    }
-//    gsl_rng_free(rng);
-//    return 0;
+   if (argc < 4) {
+        fprintf(stderr, "Usage: %s <board size> <pop size> <max iter>\n", argv[0]);
+        return 0;
+    }
+
+    /* Set up random number generation */
+    rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng, time(0));
+
+    /* Input arguments */
+    filename = argv[1];
+    pop_size = atoi(argv[2]);
+    max_iter = atoi(argv[3]);
+
+    /* Problem and solver instantiation */
+    p = newProblem(filename);
+    if (p != NULL) {
+        ss = newSolver(p, pop_size);
+
+        /* Run */
+        getObjectiveVector(&mincost, getSolverSolution(ss));
+        printf("iter = 0, obj = %.0f\n", mincost);
+        for (i = 0; mincost > 0 && i < max_iter; i++) {
+            nextSolverState(ss);
+            getObjectiveVector(&cost, getSolverSolution(ss));
+            if (cost < mincost) {
+                mincost = cost;
+                printf("iter = %d, obj = %.0f\n", i+1, mincost);
+            }
+        }
+
+        /* Report result */
+        printSolution(getSolverSolution(ss));
+
+        /* Clean up */
+        freeSolver(ss);
+        freeProblem(p);
+    }
+    gsl_rng_free(rng);
+    return 0;
 }
 
 
